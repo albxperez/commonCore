03fe525f18729c58d500f031697eb01ea4053a4f
Alba Perez Rubi <aperez-r@c2r12s5.42madrid.com>
2024-09-23 12:42:41 +0200

    libft

---
+++ libft/memset.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memset.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aperez-r <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 18:28:08 by aperez-r          #+#    #+#             */
+/*   Updated: 2024/09/23 12:14:48 by aperez-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char *ptr = (unsigned char*) s;
+
+	int i;
+
+	i = 0;
+	while(i != '\0')
+	{
+		while(i <= n)
+		{
+			ptr[i] = (unsigned char) c;
+			i++;
+		}
+	}
+
+	return ptr;
+}
+
+int	main(void)
+{
+	char str[15];
+	//printf("%p", ft_memset(str,'A', sizeof(char)));
+	ft_memset(str, 'A', 14*sizeof(char));
+	printf("%s",str);
+	memset(str, 'A',14*sizeof(char));
+	printf("\n%s", str);
+	return 0;
+}
