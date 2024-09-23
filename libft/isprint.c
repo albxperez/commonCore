a8c2290480379b2d4764c7f58348e384e459da05
Alba Perez Rubi <aperez-r@c2r12s5.42madrid.com>
2024-09-23 13:25:37 +0200

    change23sept

--- libft/isprint.c
+++ libft/isprint.c
@@ -6,7 +6,7 @@
 /*   By: aperez-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 16:49:46 by aperez-r          #+#    #+#             */
-/*   Updated: 2024/09/18 16:55:42 by aperez-r         ###   ########.fr       */
+/*   Updated: 2024/09/23 13:23:04 by aperez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,15 @@
 
 int	isprint(int c)
 {
-
+	if(c > 31 && c < 127)
+		return 0;
+	else 
+		return 1;
 }
 
 int	main(void)
 {
-	unsigned char c = ;
+	unsigned char c ='A' ;
 	printf("%d", isprint(c));
 	return 0;
 }
