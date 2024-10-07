171893f4ead46f7fef059af6e58f03c53d98d05a
albxperez <aperez-r@c2r11s6.42madrid.com>
2024-10-07 21:05:39 +0200

    split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -5,99 +5,108 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aperez-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/06 19:13:07 by aperez-r          #+#    #+#             */
-/*   Updated: 2024/10/07 12:49:47 by aperez-r         ###   ########.fr       */
+/*   Created: 2024/10/07 19:15:25 by aperez-r          #+#    #+#             */
+/*   Updated: 2024/10/07 20:59:15 by aperez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_count_words(char const *s, char c)
+int	count_words(const char *s, char c)
 {
-	int	count;
-	int	in_word;
+	int	words;
 	int	i;
 
-	count = 0;
-	in_word = 0;
+	words = 0;
 	i = 0;
-       	while (s[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (s[i] != c && in_word == 0)
+		while (s[i] == c)
+			s++;
+		if (s[i] != '\0')
 		{
-			in_word = 1;
-			count++;
+			words++;
+			while (s[i] != '\0' && s[i] != c)
+				s++;
 		}
-		else if (s[i] == c)
-		{
-			in_word = 0;
-		}
-		i++;
 	}
-    return count;
+	return (words);
 }
 
-char	*ft_word_dup(const char *s, int start, int end)
+char	*ft_word_dup(const char *s, char c)
 {
-	int	i;
+	int		len;
+	char	*string;
 
-	i = 0;
-	char *word = (char *)malloc((end - start + 1)*sizeof(char *));
-	if(word == NULL)
-		return NULL;
-	while(start < end)
-	{
-		word[i++] = s[start++];
-	}
-	word[i] = '\0';
-	return word;
+	len = 0;
+	while (s[len] != '\0' && s[len] != c)
+		len ++;
+	string = (char *)malloc(len + 1);
+	if (string == NULL)
+		return (NULL);
+	ft_strlcpy(string, s, len + 1);
+	return (string);
+}
+
+void ft_free_split(char **result) {
+    int i = 0;
+
+    if (result == NULL)
+        return; // No hacer nada si result es NULL
+
+    while (result[i] != NULL) {
+        free(result[i]); // Liberar cada palabra
+        i++;
+    }
+    free(result); // Liberar el array de punteros
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
-	int	count_words;
-	size_t	i;
-	size_t	j;
-	int	start;
+	int		i;
+	int		j;
 
-	count_words = ft_count_words(s, c);
+	if (s == NULL)
+		return (NULL);
+	result = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (result == NULL)
+		return (NULL);
 	i = 0;
 	j = 0;
-	start = -1;
-	result = (char **)malloc((count_words + 1)*sizeof(char *));
-	if(result == NULL)
-		return NULL;
 	while (s[i] != '\0')
 	{
-		if(s[i] != c && start == -1)
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
 		{
-			start = i;
+			result[j] = ft_word_dup(&s[i], c);
+			if(result[j] == NULL)
+			{
+				ft_free_split(result);
+				return NULL;
+			}
+			j++;
+			while (s[i] != '\0' && s[i] != c)
+				s++;
 		}
-		else if((s[i] == c || s[i + 1] == '\0') && start != -1)
-		{
-			if(s[i] == c)
-				result[j++] = ft_word_dup(s, start, i);
-			else
-				result[j++] = ft_word_dup(s, start, i + 1);
-		start = -1;
-		}
-	i++;
 	}
 	result[j] = NULL;
-	return result;
+	return (result);
 }
 
-/*int	main(void)
+/*int main(void)
 {
-	char string[] = "hola que tal estas está mañana";
-	char c = 'a';
-	char **result = ft_split(string, c);
-	for (int i = 0; result[i] != NULL; i++) 
-	{
-        	printf("Palabra %d: %s\n", i + 1, result[i]);
-        	free(result[i]);  // Liberamos la memoria de cada palabra
-   	}
-    	free(result);
+    char string[] = "lorem ipsum dolor sit amet, consectetur adipiscing elits.";
+    char c = 'i'; // Delimitador
+    char **result = ft_split(string, c);
 
+    for (int i = 0; result[i] != NULL; i++)
+    {
+        printf("Palabra %d: %s\n", i + 1, result[i]);
+        free(result[i]); // Liberamos la memoria de cada palabra
+    }
+    free(result); // Liberamos la memoria del array
+
+    return 0;
 }*/
