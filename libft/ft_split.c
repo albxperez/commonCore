7725b55078c3b8257b50bea09bdb9bbc1200886e
albxperez <aperez-r@c2r15s3.42madrid.com>
2024-10-08 11:24:57 +0200

    finished

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: aperez-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 19:15:25 by aperez-r          #+#    #+#             */
-/*   Updated: 2024/10/08 08:56:59 by aperez-r         ###   ########.fr       */
+/*   Updated: 2024/10/08 11:12:25 by aperez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,31 @@ static int	count_words(const char *s, char c)
 	return (words);
 }
 
-static void ft_free_split(char **result, int letters)
+static int	len_words(const char *s, char c, int i)
+{
+	int	len;
+
+	len = 0;
+	while (s[i] != '\0' && s[i] != c)
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+static void	ft_free_split(char **result, int letters)
 {
 	int	i;
 
 	i = 0;
 	if (result == NULL)
-		return;
+		return ;
 	while (i < letters)
 	{
 		free(result[i]);
 		i++;
 	}
-	free(result);
 }
 
 static int	ft_fill_split(char **result, const char *s, char c)
@@ -53,41 +65,43 @@ static int	ft_fill_split(char **result, const char *s, char c)
 	int	i;
 	int	len;
 	int	word;
+
 	i = 0;
 	word = 0;
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if(s[i] == c)
+		if (s[i] == c)
 			i++;
 		else
 		{
-			while (s[len] != '\0' && s[len] != c)
-				len ++;
+			len = len_words(s, c, i);
 			result[word] = (char *)malloc(len + 1);
-			if (result == NULL)
+			if (result[word] == NULL)
 			{
 				ft_free_split(result, word);
-				return (0);
+				return (-1);
 			}
-
-			ft_strlcpy(*result, s, len + 1);
+			ft_strlcpy(result[word], &s[i], len + 1);
 			i += len;
 			word ++;
 		}
 	}
-	return (1);
+	return (0);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
+	int		words;
 
+	words = count_words(s, c);
 	if (s == NULL)
 		return (NULL);
-	result = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
+	result = (char **)malloc((words + 1) * sizeof(char *));
 	if (result == NULL)
 		return (NULL);
-	if(ft_fill_split(result, s, c) == 0)
+	result[words] = NULL;
+	if (ft_fill_split(result, s, c) == -1)
 	{
 		free(result);
 		return (NULL);
@@ -95,10 +109,10 @@ char	**ft_split(char const *s, char c)
 	return (result);
 }
 
-int main(void)
+/*int main(void)
 {
-    char string[] = "lorem ipsum dolor sit amet, consectetur adipiscing elits.";
-    char c = 'i'; // Delimitador
+    char string[] = "lorem ipsum dolor sit amet, consectetur adipiscing elits .";
+    char c = ' '; // Delimitador
     char **result = ft_split(string, c);
 
     for (int i = 0; result[i] != NULL; i++)
@@ -109,4 +123,4 @@ int main(void)
     free(result); // Liberamos la memoria del array
 
     return 0;
-}
+}*/
